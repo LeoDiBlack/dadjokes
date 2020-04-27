bba6bc45aaa98722775085b5851f21af0e00318b
leonardo.ardila <leonardo.ardila@zemoga.com>
2020-04-27 14:56:05 -0500

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>About DadJokes</h1>
+    <p>This is an app that displays corny dad jokes</p>
+  </div>
+</template>
+
+<script>
+export default {
+	head() {
+		return {
+			title: 'About the app',
+			meta: [
+				{
+					hid: 'description',
+					name: 'description',
+					content: 'Best place for corny bad jokes'
+				}
+			]
+		}
+	}
+};
+</script>
+
+<style>
+</style>
